c1dc7671d5883005dfbe8b24a7c3f0d121df7965
yugandhar99 <104614963+yugandhar99@users.noreply.github.com>
2023-08-02 06:21:50 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('mousemove', e => {
+  let x = e.clientX;
+  let y = e.clientY; 
+  let message = "Position X: " + x 
+    + ", Position Y: " + y; document.getElementById('output').innerHTML = message;
+});
